7139b974ee616cd4400a834aa18db470a354caff
Jordan Bell <47544607+jordanbell2357@users.noreply.github.com>
2023-05-29 17:56:49 -0400

    Update MarineCadastre_CSV.sh

--- MarineCadastre_CSV.sh
+++ MarineCadastre_CSV.sh
@@ -1,28 +1,17 @@
-for i in {01..30}; do \
-curl -O https://coast.noaa.gov/htdata/CMSP/AISDataHandler/2022/AIS_2022_06_${i}.zip; \
-unzip AIS_2022_06_${i}.zip; \
-rm AIS_2022_06_${i}.zip;
-done
-
-for i in {01..30}; do \
-gsutil cp AIS_2022_06_${i}.csv gs://jordanbell2357marinecadastre/; \
-rm AIS_2022_06_${i}.csv; \
-done
-
-bq mk --table --schema=MarineCadastre_schema.json uscg.nais
-
-bq show --schema --format=prettyjson ais-data-385301:uscg.nais
-bq show --schema --format=prettyjson ais-data-385301:uscg.nais | diff MarineCadastre_schema.json -
-
-for i in {01..30}; do \
+y='2022'
+m='06'
+for d in {01..30}; do \
+curl -O https://coast.noaa.gov/htdata/CMSP/AISDataHandler/${y}/AIS_${y}_${m}_${d}.zip; \
+unzip AIS_${y}_${m}_${d}.zip; \
+rm AIS_${y}_${m}_${d}.zip; \
+gsutil cp AIS_${y}_${m}_${d}.csv gs://jordanbell2357marinecadastre/; \
+rm AIS_${y}_${m}_${d}; \
 bq load \
 --source_format=CSV \
 --skip_leading_rows=1 \
 --max_bad_records=200 \
 --schema=MarineCadastre_schema.json \
 uscg.nais \
-gs://jordanbell2357marinecadastre/AIS_2022_06_${i}.csv; \
+gs://jordanbell2357marinecadastre/AIS_${y}_${m}_${d}; \
+gsutil rm gs://jordanbell2357marinecadastre/AIS_${y}_${m}_${d}.csv; \
 done
-
-bq query --use_legacy_sql=false 'SELECT COUNT(*) FROM ais-data-385301.uscg.nais;'
-# 249325885
